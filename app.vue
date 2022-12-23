d58298d6d506dff0f68a02902668b51b8b90edba
Yuta Toyokawa <kppg42@gmail.com>
2022-12-24 00:15:54 +0900

    レイアウトが2重に作られるバグの修正

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
-<GlobalHeader></GlobalHeader>
-<div class="wrapper">
-  <Sidebar></Sidebar>
-  <RecentPost></RecentPost>
-  <Post></Post>
-  <Post></Post>
-  <Post></Post>
+<div>
+  <GlobalHeader></GlobalHeader>
+  <div class="wrapper">
+    <Sidebar></Sidebar>
+    <RecentPost></RecentPost>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+  </div>
+  <GlobalFooter></GlobalFooter>
 </div>
-<GlobalFooter></GlobalFooter>
 </template>
