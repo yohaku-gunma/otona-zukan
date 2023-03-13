63d7c9fc9b779c7a06071c3d48ca58f3d1e47e5c
Yuika2003 <yui2003ka@gmail.com>
2023-03-13 20:01:06 +0900

    キャッチコピーの追加

---
+++ components/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <div class="img">
+   <h1>メインキャッチコピー</h1>
+   <h2>サブキャッチコピー</h2>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.img {
+  position: relative;
+  height: 1002px;
+  margin-left: 40px;
+  margin-top: 60px;
+  margin-bottom: 80px;
+  background-color: #C4C4C4;
+}
+
+h1 {
+  position: absolute;
+  bottom: 168px;
+  left: 90px;
+  color: #000000;
+  font-size: 64px;
+}
+
+h2 {
+  position: absolute;
+  bottom: 77px;
+  left: 90px;
+  color: #000000;
+  font-size: 48px;
+}
+</style>
